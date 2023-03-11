c35f4a31e0f1c973d16cbc31eda70a08a726b573
Shane Carroll <scarroll825@gmail.com>
2023-03-11 13:07:24 -0500

    Add hover event and button to set grid size

--- scripts.js
+++ scripts.js
@@ -1,7 +1,19 @@
 const container = document.querySelector('.container');
+const button = document.querySelector('button');
 
-for (i = 0; i < 256; i++) {
-    const div = document.createElement('div');
-    div.classList.add('box');
-    container.appendChild(div);
-};
+button.addEventListener('click', () => {
+    let response = +prompt("How many squares per column/row?");
+    if (response <= 100) {
+        container.style.gridTemplateColumns = `repeat(${+response}, 1fr)`;
+        container.style.gridTemplateRows = `repeat(${+response}, 1fr)`;
+        container.innerHTML = '';
+        for (i = 0; i < (response ** 2); i++) {
+            const div = document.createElement('div');
+            div.classList.add('box');
+            container.appendChild(div);
+            div.addEventListener('mouseover', function (e) {
+                e.target.style.backgroundColor = 'yellow';
+            });
+        }        
+    }
+})
